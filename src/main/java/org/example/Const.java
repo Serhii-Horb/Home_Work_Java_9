98a7e9b741778a89761ce51c409666fc0b7b48af
Serhii Horb <serhii.horb.ua@gmail.com>
2024-01-14 20:26:55 +0100

    hw_9

---
+++ src/main/java/org/example/Const.java
@@ -0,0 +1,1 @@
+package org.example;public class Const {    public static final Integer WIN_NUM = 15;    public static final Integer USER_NUM = 5;    public static final Integer RANGE = 50;}
