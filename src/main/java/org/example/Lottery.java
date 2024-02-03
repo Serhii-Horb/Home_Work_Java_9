3f354dd48060f65bc4dd0d6696c59f720a030cb8
Serhii Horb <43185034+Serhii-Horb@users.noreply.github.com>
2024-02-03 16:18:42 +0100

    Update Lottery.java

--- src/main/java/org/example/Lottery.java
+++ src/main/java/org/example/Lottery.java
@@ -1,1 +1,22 @@
-package org.example;import java.util.Scanner;import static org.example.Util.*;public class Lottery {    public static void startLottery() {        Scanner sc = new Scanner(System.in);        System.out.println("Введите количество игроков: ");        int playerCounter = sc.nextInt();        Integer[] winNums = createWinData();        Integer[] winArrey;        for (int i = 0; i < playerCounter; i++) {            Integer[] userNums = inputUserData();            winArrey = winNums;            printResult(userNums, winArrey);        }    }}
+package org.example;
+
+import java.util.Scanner;
+
+import static org.example.Util.*;
+
+public class Lottery {
+    public static void startLottery() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите количество игроков: ");
+        int playerCounter = sc.nextInt();
+
+        Integer[] winNums = createWinData();
+        Integer[] winArrey;
+
+        for (int i = 0; i < playerCounter; i++) {
+            Integer[] userNums = inputUserData();
+            winArrey = winNums;
+            printResult(userNums, winArrey);
+        }
+    }
+}
