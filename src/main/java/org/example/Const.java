0b03500aa490c8f73a8e7fa9545881e8663d7172
Serhii Horb <43185034+Serhii-Horb@users.noreply.github.com>
2024-02-03 16:18:20 +0100

    Update Const.java

--- src/main/java/org/example/Const.java
+++ src/main/java/org/example/Const.java
@@ -1,1 +1,8 @@
-package org.example;public class Const {    public static final Integer WIN_NUM = 15;    public static final Integer USER_NUM = 5;    public static final Integer RANGE = 50;}
+package org.example;
+
+public class Const {
+    public static final Integer WIN_NUM = 15;
+    public static final Integer USER_NUM = 5;
+    public static final Integer RANGE = 50;
+
+}
